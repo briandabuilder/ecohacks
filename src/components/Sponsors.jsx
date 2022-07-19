bfff8d10b1f7f369d6eed76cbdee9f9dbe5dd6e5
RedBlueBird <jiefu.zhang1226@gmail.com>
2022-07-18 21:10:00 -0700

    added wolfram sponsor

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -6,7 +6,7 @@ function Sponsors() {
     return (
         <div id="sponsors" className="my-5">
             <div className="d-flex flex-column align-items-center mx-3">
-                <div className="m-1 my-5" style={{ color: "#FFD700", fontFamily: "Amatic SC, cursive", fontSize: "4rem", textShadow: "0 0 25px #FFD700" }}>
+            {/*<div className="m-1 my-5" style={{ color: "#FFD700", fontFamily: "Amatic SC, cursive", fontSize: "4rem", textShadow: "0 0 25px #FFD700" }}>
                     Gold Sponsors
                 </div>
                 <FadeIn className="d-flex flex-row flex-wrap justify-content-around mb-3">
@@ -34,15 +34,16 @@ function Sponsors() {
                         alt="..."
                     />
                 </FadeIn>
-
+            */}
 
                 <div className="m-1 my-5" style={{ color: "#CD7F32", fontFamily: "Amatic SC, cursive", fontSize: "4rem", textShadow: "0 0 25px #CD7F32" }}>
                     Bronze Sponsors
                 </div>
                 <FadeIn className="d-flex flex-row flex-wrap justify-content-center mb-3">
                     <img
-                        src="https://via.placeholder.com/200x200.png?text=TBD"
+                        src={require("../images/wolfram.png")}
                         alt="..."
+                        style={{height: "10em"}}
                     />
                 </FadeIn>
             </div>
