aaae3cf5b41cc46696f1ad4d231f6ea155667397
bdonyan <66976326+bdonyan@users.noreply.github.com>
2022-08-03 21:11:59 -0700

    Update FAQ.jsx

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -13,7 +13,7 @@ function FAQ() {
             response: "A hackathon is an event where students come together to design and build awesome projects for a period of time. A great opportunity for students to gather up, learn, and make friends!"
         }, {
             question: "Who is allowed to participate?",
-      response: "Anyone is welcome to participate. However, only participants age 13 and above are eligible for prizes. If a winning participant is living outside of the US and the physical prizes cannot be shipped to their location, they will be notified ahead of time through [email](mailto:eco.hacks.22@gmail.com) and/or [discord](https://discord.gg/2EvZkYEeyk)."
+      response: "Anyone age 13 and up are elegible to compete and earn prizes. If a winning participant is living outside of the US and the physical prizes cannot be shipped to their location, they will be notified ahead of time through [email](mailto:eco.hacks.22@gmail.com) and/or [discord](https://discord.gg/2EvZkYEeyk)."
         }, {
             question: "How many people are allowed on each team?",
             response: "A team should consist of 1-4 people."
@@ -25,7 +25,7 @@ function FAQ() {
             response: "No worries at all! Here is a list of resources to get you started: \n\t [1] [Github student developer pack](https://education.github.com/pack), limitless free resources. \n\t [2] [Machine Learning Specialization](https://www.deeplearning.ai/courses/machine-learning-specialization/), a new revamped AI course for beginners by pioneer Andrew Ng. \n\t [3] [How to survive a hackathon](https://hackernoon.com/how-to-survive-a-hackathon-41318e925d32), quick guide to keep sanity in check under a competitive environment!"
         }, {
             question: "What is the format of the hackathon?",
-            response: "Our hackathon runs online from 9/3 - 9/5. Anyone age 13 and up who are interested in learning about coding and environmental issues are allowed to participate. Each participant can form a team of 1 to 4 people. Teams will be given a theme at the start of the hackathon, where they will use to develop a project through the medium of their choice. At the end, each team should attach a GitHub link to their product as well as a video presentation describing the product and its purpose. Open sourced packages are permitted, but no code should be written prior to the hackathon."
+            response: "Our hackathon runs online from 9/2 - 9/4. Anyone age 13 and up who are interested in learning about coding and environmental issues are allowed to participate. Each participant can form a team of 1 to 4 people. Teams will be given a theme at the start of the hackathon, where they will use to develop a project through the medium of their choice. At the end, each team should attach a GitHub link to their product as well as a video presentation describing the product and its purpose. Open sourced packages are permitted, but no code should be written prior to the hackathon."
         }, {
             question: "Do we get to keep our projects after the event ends?",
             response: "Yes! The project you built is yours to keep. We will only have the permission to display your work and ideas on our website."
